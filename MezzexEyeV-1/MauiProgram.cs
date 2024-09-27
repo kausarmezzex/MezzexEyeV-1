a43114883b3a085ed58cb2a85cc4761d8a97ced8
mezzexmaz@outlook.com <mezzexmaz@outlook.com>
2024-09-27 18:12:08 +0530

    g

--- MezzexEyeV-1/MauiProgram.cs
+++ MezzexEyeV-1/MauiProgram.cs
@@ -1,33 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Maui.LifecycleEvents;
-using System.Diagnostics;
-using System.IO;
-using System.Reflection;
+using Microsoft.Maui.Hosting;
 using System.Runtime.InteropServices;
-using System.Threading;
 using MezzexEyeV_1.Platforms.Windows;
-
 namespace MezzexEyeV_1
 {
     public static class MauiProgram
     {
-        // Global mutex for single instance prevention
-        private static Mutex _mutex = null;
-
         public static MauiApp CreateMauiApp()
         {
-            // Use a unique identifier for your application
-            const string appMutexName = "Global\\MyMauiBlazorAppMutex";
-
-            // Ensure only one instance is running
-            _mutex = new Mutex(true, appMutexName, out bool isNewInstance);
-
-            if (!isNewInstance)
-            {
-                // If there is already an instance running, exit the new instance
-                return null; // This will prevent the second instance from being created.
-            }
-
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -36,20 +17,45 @@ namespace MezzexEyeV_1
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
-            // Register Blazor WebView
             builder.Services.AddMauiBlazorWebView();
 
+            // Register HttpClient to be used for API calls
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7045/") // Base URL for your API
+            });
+
+
 #if DEBUG
+            // Add developer tools during debugging
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
+#endif
+
+            // Inside your MauiProgram class
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if WINDOWS
+                events.AddWindows(windows =>
+                {
+                    windows.OnWindowCreated(window =>
+                    {
+                        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowHelper.DisableWindowButtons(hWnd);
+                        WindowHelper.SetFullScreenMode(hWnd, true);
+                    });
+                });
+#endif
+            });
+
+#if WINDOWS
+            builder.Services.AddSingleton<ScreenshotCaptureService>();
 #endif
 
 #if WINDOWS
             // Register the service that will create a shortcut in the Startup folder
             builder.Services.AddSingleton<StartupShortcutService>();
 #endif
-
-            // Add Windows lifecycle events
             builder.ConfigureLifecycleEvents(events =>
             {
 #if WINDOWS
